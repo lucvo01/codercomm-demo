46e971138859cfbef649caa70ff3535144131d90
lucvo01 <109111777+lucvo01@users.noreply.github.com>
2023-05-16 20:09:33 +0000

    .

--- src/layouts/BlankLayout.js
+++ src/layouts/BlankLayout.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import {Stack} from '@mui/material'
 
 function BlankLayout() {
-  return <div>BlankLayout</div>;
+  return (
+    <Stack minHeight="100vh" justifyContent="center" alignItems='center'>
+      <h1>BlankLayout</h1>
+      <Outlet />
+    </Stack>
+  );
 }
 
 export default BlankLayout;
