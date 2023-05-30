f6d6ead49e218375b324a92c0d16c5f9fd614931
lucvo01 <109111777+lucvo01@users.noreply.github.com>
2023-05-30 21:57:50 +0000

    May 30, 2023, 3:57 PM

--- src/features/comment/CommentCard.js
+++ src/features/comment/CommentCard.js
@@ -1,7 +1,32 @@
+import { Box, Stack, Typography, Avatar, Paper } from "@mui/material";
 import React from "react";
+import { fDate } from "../../utils/formatTime";
 
 function CommentCard({comment}) {
-  return (<div>{comment.content}</div>);
+  return (<Stack direction='row' spacing={2}>
+    <Avatar alt={comment.author?.name} src={comment.author?.avatarUrl}/>
+    <Paper sx={{p: 1.5, flexGrow: 1, bgcolor: "background.neutral"}} >
+        <Stack 
+        direction='row'
+        alignItems={{sm: 'center'}}
+        justifyContent="space-between"
+        sx= {{mb: 0.5}}
+        >
+            <Typography variant="subtitle2" sx={{fontWeight: 600}}>
+              {comment.author?.name}
+            </Typography>
+            <Typography variant="caption" sx={{color: 'text.disabled'}}>
+                    {fDate(comment.createAt)}
+            </Typography>
+        </Stack>
+        <Typography variant='body2' sx={{color: 'text.secondary'}}>
+            {comment.content}
+        </Typography>
+        <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
+            {/* <CommentReaction comment={comment}/> */}
+        </Box>
+    </Paper>
+  </Stack>);
 }
 
 export default CommentCard;
