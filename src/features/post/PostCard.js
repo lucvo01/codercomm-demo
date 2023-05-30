46a3e3b3c2209aa2d78ef963eedd73eca6d72452
lucvo01 <109111777+lucvo01@users.noreply.github.com>
2023-05-30 20:02:14 +0000

    May 30, 2023, 2:02 PM

--- src/features/post/PostCard.js
+++ src/features/post/PostCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Box, Link, Card, Stack, Avatar, Typography, CardHeader, IconButton} from '@mui/material'
 
 function PostCard({ post }) {
   return <div>{post.content}</div>;
