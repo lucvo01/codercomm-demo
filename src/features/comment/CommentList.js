4a803b244033a2ce062315f42d850029d21d9a26
lucvo01 <109111777+lucvo01@users.noreply.github.com>
2023-05-30 21:28:25 +0000

    May 30, 2023, 3:28 PM

--- src/features/comment/CommentList.js
+++ src/features/comment/CommentList.js
@@ -1,9 +1,28 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import { getComments } from "./commentSlice";
 
 function CommentList({postId}) {
     const dispatch = useDispatch();
     
+    const {
+        commentsByPost,
+        commentsById,
+        totalComments,
+        isLoading,
+        currentPage
+    } = useSelector(
+        (state) => ({
+            commentsByPost: state.comment.commentsByPost[postId],
+            totalComments: state.comment.totalComments[postId],
+            currentPage: state.comment.currentPage[postId],
+            commentsById: state.comment.commentsById,
+            isLoading: state.comment.isLoading,
+            
+        }),
+        shallowEqual
+    );
+
 useEffect(() => {
     if (postId) dispatch(getComments({postId}));
 }, [postId, dispatch])
