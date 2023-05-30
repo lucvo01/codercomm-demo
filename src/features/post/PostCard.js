31456879164d0aed9e9c26a44728d3c03dd329d6
lucvo01 <109111777+lucvo01@users.noreply.github.com>
2023-05-30 20:13:55 +0000

    May 30, 2023, 2:13 PM

--- src/features/post/PostCard.js
+++ src/features/post/PostCard.js
@@ -1,11 +1,33 @@
 import React from "react";
-import {Box, Link, Card, Stack, Avatar, Typography, CardHeader, IconButton} from '@mui/material'
+import {Box, Link, Card, Stack, Avatar, Typography, CardHeader, IconButton, MoreVertIcon} from '@mui/material'
 import{Link as RouterLink} from 'react-router-dom'
+import {fDate} from '../../utils/formatTime';
+import { MoreVert } from "@mui/icons-material";
+
 
 function PostCard({ post }) {
   return (<Card>
-    <CardHeader>
-      
+    <CardHeader
+    disableTypography
+    avatar={
+      <Avatar src={post?.author?.avatarUrl} alt={post?.author?.name}/>
+    }
+    title= {<Link variant="subtitle2" color="text.primary" component={RouterLink} sx={{fontWeight: 600}} to={`/user/${post.author._id}`}>
+      {post?.author?.name}
+
+    subheader={
+      <Typography variant="caption" sx={{display: "block", color: 'text.secondary'}}>
+        {fDate([post.createAt])}
+      </Typography>
+    }
+
+    action={
+      <IconButton>
+        <MoreVertIcon sx={{ fontSize: 30}}/>
+      </IconButton>
+    }
+    </Link>}
+    >
     </CardHeader>
     </Card>);
 }
