87cb6e07fe2aae86ada6201fa1e59d7601bc3764
lucvo01 <109111777+lucvo01@users.noreply.github.com>
2023-05-30 20:03:57 +0000

    May 30, 2023, 2:03 PM

--- src/features/post/PostCard.js
+++ src/features/post/PostCard.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {Box, Link, Card, Stack, Avatar, Typography, CardHeader, IconButton} from '@mui/material'
+import{Link as RouterLink} from 'react-router-dom'
 
 function PostCard({ post }) {
-  return <div>{post.content}</div>;
+  return <Card>{post.content}</Card>;
 }
 
 export default PostCard;
