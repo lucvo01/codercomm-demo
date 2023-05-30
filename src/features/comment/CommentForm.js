021c70298f043a3fc3a9d1e1abd729d9f9d79523
lucvo01 <109111777+lucvo01@users.noreply.github.com>
2023-05-30 20:49:57 +0000

    May 30, 2023, 2:49 PM

--- src/features/comment/CommentForm.js
+++ src/features/comment/CommentForm.js
@@ -1,7 +1,42 @@
-import React from "react";
+import { Avatar, IconButton, Stack, TextField } from "@mui/material";
+import React,  {useState} from "react";
+import {SendIcon} from '@mui/icons-material'
+import useAuth from '../../hooks/useAuth'
 
 function CommentForm() {
-  return (<div><h6>CommentForm</h6></div>);
+    const [content, setContent] = useState("");
+    const {user} = useAuth();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(content)
+    }
+
+
+  return (
+  <form onSubmit={handleSubmit}>
+    <Stack direction='row' alignItems='center'>
+        <Avatar src={user.avatarUrl} alt={user.name}/>
+        <TextField 
+        fullWidth
+        size="small"
+        value={content}
+        placeholder="Write a comment..."
+        onChange={(event) => setContent(event.target.value)}
+        sx={{
+            ml: 2,
+            mr: 1,
+            "& fieldset": {
+                borderWidth: `1px !important`,
+                borderColor: (theme) => `${theme.palette.grey[500_32]} !important`,
+            }
+        }}
+        />
+        <IconButton>
+            <SendIcon sx={{fontSize: 30}}/>
+        </IconButton>
+    </Stack>
+    </form>);
 }
 
 export default CommentForm;
