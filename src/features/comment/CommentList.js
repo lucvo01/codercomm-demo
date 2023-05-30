582af5ab453ddf9d6fe75ebddf9d03332f0fb8b9
lucvo01 <109111777+lucvo01@users.noreply.github.com>
2023-05-30 21:34:36 +0000

    May 30, 2023, 3:34 PM

--- src/features/comment/CommentList.js
+++ src/features/comment/CommentList.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { getComments } from "./commentSlice";
+import { COMMENTS_PER_POST } from "../../app/config";
+import { Pagination, Stack, Typography } from "@mui/material";
 
 function CommentList({postId}) {
     const dispatch = useDispatch();
@@ -15,7 +17,7 @@ function CommentList({postId}) {
         (state) => ({
             commentsByPost: state.comment.commentsByPost[postId],
             totalComments: state.comment.totalComments[postId],
-            currentPage: state.comment.currentPage[postId],
+            currentPage: state.comment.currentPage[postId] || 1,
             commentsById: state.comment.commentsById,
             isLoading: state.comment.isLoading,
             
@@ -23,13 +25,31 @@ function CommentList({postId}) {
         shallowEqual
     );
 
+    const totalPages = Math.ceil(totalComments / COMMENTS_PER_POST);
+
 useEffect(() => {
     if (postId) dispatch(getComments({postId}));
 }, [postId, dispatch])
 
-  return (<div>
-    <h6>CommentList</h6>
-    </div>);
+  return (<Stack spacing={1.5}>
+    <Stack direction='row' justifyContent='space-between'>
+        <Typography variant="subtitle" sx={{color: 'text.secondary'}}>
+            {totalComments > 1 
+            ? `${totalComments} comments`
+            : totalComments === 1
+            ? `${totalComments} comment`
+            : "No comment"
+        }
+        </Typography>
+        {totalComments > COMMENTS_PER_POST && (
+            <Pagination  
+            count={totalPages}
+            page={currentPage}
+            onChange={ (e, page) => dispatch(getComments({postId, page}))}
+            />
+        )}
+    </Stack>
+    </Stack>);
 }
 
 export default CommentList;
