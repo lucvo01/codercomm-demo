442b77ce54b9f34494463244f3bc2d6178549508
Vo <Luc.Vo@encana.com>
2023-05-31 12:24:51 -0600

    /

--- src/features/friend/UserTable.js
+++ src/features/friend/UserTable.js
@@ -1,25 +1,72 @@
 import React from "react";
 import {
-    Table,
-    TableHead,
-    Avatar,
-    TableRow,
-    TableBody,
-    TableCell,
-    Link,
-    TableContainer,
-    Box
-} from '@mui/material';
+  Table,
+  TableHead,
+  Avatar,
+  TableRow,
+  TableBody,
+  TableCell,
+  Link,
+  TableContainer,
+  Box,
+} from "@mui/material";
 import useAuth from "../../hooks/useAuth";
 
-function UserTable({users}) {
-    const {user} = useAuth();
-    const currentUserId = user._id;
+function UserTable({ users }) {
+  const { user } = useAuth();
+  const currentUserId = user._id;
 
   return (
-    <div>
-      <h6>UserTable: {users?.length}</h6>
-    </div>
+    <Box sx={{ overflowX: "auto" }}>
+      <TableContainer sx={{ minWidth: 800 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ width: { xs: "20%", sm: "25%" } }}>
+                Name
+              </TableCell>
+              <TableCell sx={{ display: { xs: "none", md: "table-cell" } }}>
+                Email
+              </TableCell>
+              <TableCell sx={{ display: { xs: "none", md: "table-cell" } }}>
+                Job Title
+              </TableCell>
+              <TableCell sx={{ display: { xs: "none", sm: "table-cell" }, width: '20%' }}>
+                Status
+              </TableCell>
+              <TableCell sx={{ width: { xs: "20%", sm: "25%" } }}>
+                Action
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {user.map((user) => {
+                return (
+                    <TableRow key={user._id} hover>
+                        <TableCell
+                            sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer'}}
+                        >
+                            <Avatar
+                                alt={user.name}
+                                src={user.avatarUrl}
+                                sx={{mr: 2}}
+                            />
+                            <Link 
+                                variant='subtitle2'
+                                sx={{fontWeight: 600}}
+                                component={RouterLink}
+                                to={`/user/${user._id}`}
+                            >
+                            {user.name}
+                            </Link>
+                        </TableCell>
+                    </TableRow>
+                )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
 
